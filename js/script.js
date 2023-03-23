c12556be6ee1bde77acc010e007754c199ba7282
pksnowstorm <enguyen21@gmail.com>
2023-03-23 10:16:11 -0500

    Change webpage and Javascript to search and upload card image

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 let cardData, userInput;
 
-const $name = $('#name');
-const $rules = $('#rules');
-const $hp = $('#hp');
-const $type = $('#type');
-const $ability = $('#ability');
-const $attack = $('#attack');
-const $weakness = $('#weakness');
-const $resistance = $('#resistance');
-const $retreat = $('#retreat');
 const $input = $('input[type="text"]');
 
 $('form').on('submit', handleGetData);
@@ -32,39 +23,11 @@ function handleGetData(event) {
 }
 
 function render() {
-  $name.text(cardData.data[0].name);
-  if (cardData.data[0].rules){
-    $rules.text(cardData.data[0].rules);
-  }else {
-    $rules.text('none')
-  }
-  $hp.text(cardData.data[0].hp);
-  $type.text(cardData.data[0].types);
-  if (cardData.data[0].abilities){
-    $ability.text(cardData.data[0].abilities[0].name + ': ' + cardData.data[0].abilities[0].text);
-  } else {
-    $ability.text('none');
-  }
-  if (cardData.data[0].attacks){
-    let attack = '';
-    for (i = 0; i < cardData.data[0].attacks.length; i++){
-      attack = attack + cardData.data[0].attacks[i].name + " " + cardData.data[0].attacks[i].cost.join(" ") + " " + cardData.data[0].attacks[i].damage + " " + cardData.data[0].attacks[i].text + " || ";
-    };
-    $attack.text(attack);
-  };
-  if (cardData.data[0].weaknesses){
-    $weakness.text(cardData.data[0].weaknesses[0].type);
-  }else {
-    $weakness.text('none');
-  };
-  if (cardData.data[0].resistances){
-    $resistance.text(cardData.data[0].resistances[0].type);
-  }else {
-    $resistance.text('none');
-  };
-  if (cardData.data[0].retreatCost){
-    $retreat.text(cardData.data[0].retreatCost.join(" "));
-  } else {
-    $retreat.text('free');
+  for (i = 0; i <cardData.data.length; i++){
+    $('#image').append('<img src="' + cardData.data[i].images.small + '">');
   };
 }
+
+$("button").click(function(){
+  $("p").empty();
+});
